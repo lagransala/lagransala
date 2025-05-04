c2cf684d287e59ff75c24bab96f24dba49781812
haztecaso <adrianlattes@disroot.org>
2025-05-04 18:07:47 +0200

    refactor(scraper): use new Pagination.urls in pagination_elements function

--- src/lagransala/scraper/application/pagination_elements.py
+++ src/lagransala/scraper/application/pagination_elements.py
@@ -1,6 +1,7 @@
 from typing import Pattern
+
 from aiohttp import ClientSession
-from lagransala.scraper.application.pagination_urls import pagination_urls
+
 from lagransala.scraper.domain.pagination import Pagination
 from lagransala.shared.application.urls import extract_urls
 
@@ -9,7 +10,7 @@ async def pagination_elements(
     session: ClientSession, pagination: Pagination, url_pattern: str | Pattern[str]
 ) -> set[str]:
     urls: set[str] = set()
-    for url in pagination_urls(pagination):
+    for url in pagination.urls():
         async with session.get(str(url)) as response:
             urls.update(extract_urls(await response.text(), url_pattern))
     return urls

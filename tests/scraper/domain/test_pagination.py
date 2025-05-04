eef88ea969f9d6bc4144830353c34adad574e642
haztecaso <adrianlattes@disroot.org>
2025-05-04 17:55:20 +0200

    test(scraper): implement tests for pagination

---
+++ tests/scraper/domain/test_pagination.py
@@ -0,0 +1,78 @@
+from typing import Iterable
+from pydantic import ValidationError
+import pytest
+from lagransala.scraper.domain.pagination import Pagination
+
+
+@pytest.fixture()
+def valid_pagination_constructor_params() -> list[dict]:
+    return [
+        {
+            "type": None,
+            "url": "https://cinema.com/films",
+        },
+        {
+            "type": "simple",
+            "url": "https://cinema.com/films?page={n}",
+            "simple_start_from": 0,
+            "limit": 10,
+        },
+        {
+            "type": "day",
+            "url": "https://cinema.com/films?date={date}",
+            "date_format": "%Y-%m-%d",
+            "limit": 31,
+        },
+        {
+            "type": "month",
+            "url": "https://cinema.com/films?month={month}",
+            "date_format": "%Y-%m",
+            "limit": 2,
+        },
+    ]
+
+@pytest.fixture()
+def paginations(valid_pagination_constructor_params: list[dict]) -> list[Pagination]:
+    return [Pagination(**params) for params in valid_pagination_constructor_params]
+
+@pytest.fixture()
+def invalid_pagination_constructor_params() -> list[dict]:
+    return [
+        {
+            "type": None,
+        },
+        {
+            "type": "simple",
+            "url": "https://cinema.com/films?page={n}",
+            "simple_start_from": 0,
+        },
+        {
+            "type": "simple",
+            "url": "https://cinema.com/films",
+            "simple_start_from": 0,
+            "limit": 10,
+        },
+        {
+            "type": "day",
+            "url": "https://cinema.com/films?date={day}",
+            "date_format": "asdasda",
+            "limit": -10,
+        },
+    ]
+
+
+def test_valid_paginations(valid_pagination_constructor_params: list[dict]):
+    for params in valid_pagination_constructor_params:
+        Pagination(**params)
+
+
+def test_invalid_paginations(invalid_pagination_constructor_params: list[dict]):
+    for params in invalid_pagination_constructor_params:
+        with pytest.raises(ValidationError):
+            Pagination(**params)
+
+def test_pagination_urls(paginations: list[Pagination]):
+    for pagination in paginations:
+        urls = pagination.urls()
+        assert isinstance(urls, Iterable), "URLs should be iterable"
+        assert not pagination.limit or len(urls) == pagination.limit, "Number of URLs should match the limit"

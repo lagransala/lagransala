dbbc6ba5ce4156c5846e33193af2e2f95fff8c3e
haztecaso <adrianlattes@disroot.org>
2025-05-04 17:27:09 +0200

    feat(scraper): implement pagination_elements function

---
+++ src/lagransala/scraper/application/pagination_elements.py
@@ -0,0 +1,15 @@
+from typing import Pattern
+from aiohttp import ClientSession
+from lagransala.scraper.application.pagination_urls import pagination_urls
+from lagransala.scraper.domain.pagination import Pagination
+from lagransala.shared.application.urls import extract_urls
+
+
+async def pagination_elements(
+    session: ClientSession, pagination: Pagination, url_pattern: str | Pattern[str]
+) -> set[str]:
+    urls: set[str] = set()
+    for url in pagination_urls(pagination):
+        async with session.get(str(url)) as response:
+            urls.update(extract_urls(await response.text(), url_pattern))
+    return urls

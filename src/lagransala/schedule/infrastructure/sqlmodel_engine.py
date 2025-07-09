cecee44966b4abed3291b98bf4e325b076548424
haztecaso <adrianlattes@disroot.org>
2025-07-09 12:17:18 +0200

    feat(schedule): create utility to initialize sqlmodel engine

---
+++ src/lagransala/schedule/infrastructure/sqlmodel_engine.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from sqlalchemy import Engine
+from sqlmodel import SQLModel, create_engine
+
+
+def initialize_sqlmodel(db_uri: str) -> Engine:
+    engine = create_engine(
+        db_uri,
+        echo=False,
+        connect_args={"check_same_thread": False},
+    )
+    SQLModel.metadata.create_all(engine)
+    return engine

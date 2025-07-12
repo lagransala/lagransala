5a60878f03078ec16f160a60a49e300ed63a8a3c
haztecaso <adrianlattes@disroot.org>
2025-07-12 14:48:08 +0200

    refactor(scraper): JsonPaginationRepo

--- src/lagransala/scraper/infrastructure/json_pagination_repo.py
+++ src/lagransala/scraper/infrastructure/json_pagination_repo.py
@@ -1,17 +1,20 @@
+import logging
 from pathlib import Path
 from typing import overload
 
 from pydantic import TypeAdapter, ValidationError
 
-from lagransala.scraper.domain.pagination import Pagination, PaginationID
+from lagransala.scraper.domain.pagination import Pagination
+
+logger = logging.getLogger(__name__)
 
 
 class JsonPaginationRepo:
 
     _adapter = TypeAdapter(list[Pagination])
 
-    def __init__(self, file: Path):
-        self._file = file
+    def __init__(self, file: Path | str):
+        self._file = file if isinstance(file, Path) else Path(file)
 
         if not self._file.exists():
             self._file.write_text("[]")
@@ -19,9 +22,10 @@ class JsonPaginationRepo:
     def add(self, pagination: Pagination) -> None:
         """Add a pagination to the repository."""
         paginations = self.get()
-        if pagination.id in {p.id for p in paginations}:
+        if pagination.venue_slug in {p.venue_slug for p in paginations}:
             paginations = [
-                p if p.id != pagination.id else pagination for p in paginations
+                p if p.venue_slug != pagination.venue_slug else pagination
+                for p in paginations
             ]
         else:
             paginations.append(pagination)
@@ -30,13 +34,13 @@ class JsonPaginationRepo:
             file.write(data)
 
     @overload
-    def get(self, id: PaginationID) -> Pagination | None: ...
+    def get(self, venue_slug: str) -> Pagination | None: ...
 
     @overload
-    def get(self, id: None = None) -> list[Pagination]: ...
+    def get(self, venue_slug: None = None) -> list[Pagination]: ...
 
     def get(
-        self, id: PaginationID | None = None
+        self, venue_slug: str | None = None
     ) -> list[Pagination] | Pagination | None:
         """Get a pagination by its ID or all paginations if ID is None."""
         with open(self._file) as file:
@@ -44,9 +48,11 @@ class JsonPaginationRepo:
                 paginations = self._adapter.validate_json(file.read())
             except ValidationError as e:
                 for error in e.errors():
-                    print(f"Error in {self._file}: " f"{error['loc']}: {error['msg']}")
-                raise ValueError(f"Invalid JSON in {self._file}: {e}") from e
-        if id:
-            return next(filter(lambda p: p.id == id, paginations), None)
+                    logger.error(
+                        "Error in %s: %s: %s", self._file, error["loc"], error["msg"]
+                    )
+                raise ValueError(f"Error validating json {self._file}: {e}") from e
+        if venue_slug:
+            return next(filter(lambda p: p.venue_slug == venue_slug, paginations), None)
         else:
             return paginations

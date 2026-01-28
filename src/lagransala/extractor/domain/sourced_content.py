50752cfbcf44d62969469b1364f1f900524d830b
haztecaso <adrianlattes@disroot.org>
2026-01-28 13:00:34 +0100

    feat(extractor): add SourcedContent domain model

---
+++ src/lagransala/extractor/domain/sourced_content.py
@@ -0,0 +1,46 @@
+import hashlib
+import logging
+from datetime import datetime
+from enum import Enum
+from typing import Any
+
+from pydantic import BaseModel, Field, HttpUrl, model_validator
+
+logger = logging.getLogger(__name__)
+
+
+class ContentFormat(Enum):
+    MD = "markdown"
+    HTML = "html"
+    EMPTY = "empty"
+
+
+class SourcedContent(BaseModel):
+    url: HttpUrl
+    content: str | None
+    fmt: ContentFormat
+    dt: datetime = Field(default_factory=datetime.now)
+
+    @model_validator(mode="after")
+    def check_content_format(self):
+        if self.fmt == ContentFormat.EMPTY and self.content is not None:
+            raise ValueError("Content must be None if format is EMPTY")
+        return self
+
+    @model_validator(mode="before")
+    @classmethod
+    def strip_content(cls, data: dict[Any, Any]):
+        if isinstance(data["content"], str):
+            data["content"] = data["content"].strip()
+        if data["content"] == "" and data["fmt"] != ContentFormat.EMPTY:
+            logger.debug(
+                "Content is empty, setting format to EMPTY for URL: %s", data["url"]
+            )
+            data["fmt"] = ContentFormat.EMPTY
+            data["content"] = None
+        return data
+
+    @property
+    def cache_key(self) -> str:
+        payload = (self.content or "").encode("utf-8")
+        return hashlib.sha256(payload).hexdigest()
